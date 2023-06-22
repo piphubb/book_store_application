76483feb06a9fa94188bcf65536eced513097082
piphubb <piphub.p16@gmail.com>
2023-06-22 14:41:21 +0700

    Drawer Bar

--- app/src/main/java/com/piphub/freepostapp_w9/MainActivity.java
+++ app/src/main/java/com/piphub/freepostapp_w9/MainActivity.java
@@ -1,12 +1,22 @@
 package com.piphub.freepostapp_w9;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.piphub.freepostapp_w9.apis.APIInterface;
 import com.piphub.freepostapp_w9.app.BaseActivity;
 import com.piphub.freepostapp_w9.data.local.UserSharePreference;
@@ -14,21 +24,39 @@ import com.piphub.freepostapp_w9.ui.auth.LoginActivity;
 import com.piphub.freepostapp_w9.ui.category.CategoryActivity;
 import com.piphub.freepostapp_w9.ui.product.ProductActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity{
     private APIInterface apiInterface;
     private Button btnOpenCategory;
+
+    private DrawerLayout drawerLayout;
+    private ImageView menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnOpenCategory = findViewById(R.id.btnListData);
-        btnOpenCategory.setOnClickListener(new View.OnClickListener() {
+        setTitle("Home Screen");
+
+//        btnOpenCategory = findViewById(R.id.btnListData);
+//        btnOpenCategory.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        //new
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
-                startActivity(intent);
+                openDrawer(drawerLayout);
             }
         });
+        //new
+
 
 //        apiInterface = APIClient.getClient().create(APIInterface.class);
 //        apiInterface.getAllCategory().enqueue(new Callback<List<CategoryResponseItem>>() {
@@ -44,6 +72,30 @@ public class MainActivity extends BaseActivity {
 //
 //            }
 //        });
+
+    }
+
+    //new
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    //new
+    public void openCategoryActivity(View view){
+        Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+        startActivity(intent);
     }
     public void openProductActivity(View view){
         Intent intent = new Intent(MainActivity.this, ProductActivity.class);
--- app/src/main/java/com/piphub/freepostapp_w9/ui/auth/LoginActivity.java
+++ app/src/main/java/com/piphub/freepostapp_w9/ui/auth/LoginActivity.java
@@ -29,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("Login");
         initView();
         progressBar.setVisibility(View.GONE);
     }
